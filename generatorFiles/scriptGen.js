b8be26f9a4eecf97e9d76909b5380df879d95d61
jaycossey <jaycossey@live.com>
2023-12-28 14:38:20 +0000

    updated styles, added test text to check styles, updated generator functions

--- generatorFiles/scriptGen.js
+++ generatorFiles/scriptGen.js
@@ -4,74 +4,89 @@ const scriptGen = (data) => {
     const {} = data;
     // TEST FUNCTIONALITY AND ASSIGN VARIABLES HERE!!!!
     return `
-// GLOBALS ---------------------------------------
+
+// GLOBALS --------------------------------------------------
 const ROOT_DIV = document.getElementById('root');
 
 // PROFILE CONTAINER 
 const profile = () => {
-const parentDiv = document.createElement('div');
-parentDiv.className = "aboutCont";
+    const parentDiv = document.createElement('div');
+    parentDiv.className = "aboutCont";
 
-const nameJobEl = document.createElement('h1');
-nameJobEl.className = "titleName";
-nameJobEl.innerText = "Ian - Software Developer"; // THIS WILL NEED CHANGE
+    const nameJobEl = document.createElement('h1');
+    nameJobEl.className = "titleName";
+    nameJobEl.innerText = "Ian - Software Developer"; // THIS WILL NEED CHANGE
 
-const profilePic = document.createElement('img');
-profilePic.classList = "profileImg";
-profilePic.alt = "Profile Picture of <name></name>"; // THIS WILL NEED CHANGE
+    const profilePic = document.createElement('img');
+    profilePic.classList = "profileImg";
+    profilePic.alt = "Profile Picture of <name></name>"; // THIS WILL NEED CHANGE
 
-const aboutText = document.createElement('p');
-aboutText.innerText = "Example Inner Text, change when generating" // THIS WILL NEED CHANGE
+    const aboutText = document.createElement('p');
+    aboutText.innerText = "Example Inner Text, change when generating" // THIS WILL NEED CHANGE
 
-parentDiv.append(nameJobEl, profilePic, aboutText);
+    parentDiv.append(nameJobEl, profilePic, aboutText);
 
-ROOT_DIV.append(parentDiv);
+    ROOT_DIV.append(parentDiv);
 }
-console.log("Test Linked");
-document.onload = profile();
 
 
-// PROJECT CONTAINER 
+// PROJECT CONTAINER ----------------------------------------
 const createDiv = (type) => {
-const div = document.createElement('div');
-div.className = type;
+    const div = document.createElement('div');
+    div.className = type;
 
-return div;
+    return div;
 }
 
 // card generator
 const generateCard = (parent, rank) => {
-const card = createDiv('card');
-card.id = "presentation" + rank;
+    const card = createDiv('card');
+    card.id = "presentation" + rank;
 
-const titleDiv = createDiv('title');
-const screenGrabDiv = createDiv('image');
-const textDiv = createDiv('text');
-const urlDiv = createDiv('url');
+    const titleDiv = createDiv('title');
+    titleDiv.innerText = "PROJECT TITLE";
 
-card.append(titleDiv, screenGrabDiv, textDiv, urlDiv);
+    const screenGrabDiv = createDiv('image');
+    const textDiv = createDiv('text');
+    textDiv.innerText = "PROJECT DESCRIPTION GOES HERE!!!!"
+    
+    const urlDiv = createDiv('url');
+    const anchor = document.createElement('a');
+    anchor.innerText = "EXAMPLE.URL.COM";
+    urlDiv.append(anchor);
 
-parent.append(card);
+    card.append(titleDiv, screenGrabDiv, textDiv, urlDiv);
+
+    parent.append(card);
 }
 
 // portfolio creation
 const createPortfolio = () => {
-const sectionDiv = createDiv('section');
-sectionDiv.className = "portfolio";
+    const sectionDiv = createDiv('section');
+    sectionDiv.className = "portfolio";
 
-for (let i = 0; i < 3; i++) {
-    generateCard(sectionDiv, i);
+    for (let i = 0; i < 3; i++) {
+        generateCard(sectionDiv, i);
+    }
+
+    ROOT_DIV.append(sectionDiv);
 }
 
-ROOT_DIV.append(sectionDiv);
-}
-
-document.onload = createPortfolio();
 
 
+// FOOTER CONTAINER ----------------------------------------
 const otherInfo = () => {
+    const footer = createDiv('footer');
 
+    
+
+    ROOT_DIV.append(footer);
 }
+
+
+profile();
+createPortfolio();
+otherInfo();
     `
 };
 
