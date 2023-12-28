3aa31e5e9427680071d488c274d672878df23a66
jaycossey <jaycossey@live.com>
2023-12-28 13:51:25 +0000

    updated color scheme in generation

--- generatorFiles/cssGen.js
+++ generatorFiles/cssGen.js
@@ -3,21 +3,21 @@ const assignHex = (color) => {
     // designate hex codes (digits only) 
     switch(color) {
         case 'Green':
-            return '088211';
+            return '65, 174, 16';
         case 'Red':
-            return '922750';
+            return '130, 13, 13';
         case 'Purple':
-            return '683265';
+            return '144, 16, 174';
         case 'Black':
-            return '000';
+            return '0, 0, 0';
         case 'White':
-            return 'FFF';
+            return '255, 255, 255';
         case 'Yellow':
-            return '939216';
+            return '229, 176, 16';
         case 'Blue':
-            return '095399';
+            return '17, 159, 204';
         default:
-            return 'ADADAD';
+            return '173, 173, 173';
     }
 }
 
@@ -26,113 +26,114 @@ const cssGen = (color) => {
     const bgColor = assignHex(color);
 
     return `
-    :root {
-        --bg-color: ${bgColor};
-    }
-    
-    body {
-        border-size: border-box;
-        text-align: center;
-        background-color: var(--bg-color);
-        color: var(--black);
-        padding: 50px;
-        font-family: monospace;
-    }
-    
-    /* PROFILE STYLES --------------------------------- */
-    
-    .aboutCont {
-        border: 8px outset white;
-        height: 200px;
-        padding: 2px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    
-    .aboutCont > img {
-        content: "\A";
-        height: 60px;
-        width: 60px;
-        border: 6px solid green;
-        background-color: black;
-        border-radius: 50%;
-    }
-    
-    div {
-        border: 2px solid red;
-    }
-    
-    .section {
-        height: 600px;
-        display: grid;
-    }
-    
-    
-    /* PORTFOLIO STYLES ------------------------------ */
-    
-    .portfolio {
-        display: grid;
-        gap: 20px;
-        grid-template-areas: 
-            'main main'
-            'second third';
-        grid-template-rows: 3fr 2fr;
-        grid-template-columns: 1fr 1fr;
-        /* justify-content: center; */
-    }
-    
-    .portfolio > .card:first-child {
-        grid-area: main;
-        height: 100%;
-    }
-    
-    .portfolio > .card:nth-child(2) {
-        grid-area: second;
-    }
-    
-    .portfolio > .card:nth-child(3) {
-        grid-area: third;
-    }
-    
-    
-    .card {
-        height: 200px;
-        /* border: 2px solid white; */
-    }
-    
-    .title {
-        height: 40px;
-        border-radius: 10px;
-    }
-    
-    .image {
-        border: 2px solid black;
-        height: 50%;
-        margin: 5px;
-        border-radius: 15px;
-    }
-    
-    .text {
-        font-size: 1rem;
-    }
-    
-    .url {
-    
-    }
-    
-    a:hover {
-        cursor: pointer;
-    }
-    
-    a:active {
-    
-    }
-    
-    a:visited {
-    
-    } `
+:root {
+    --bg-color: rgba(${bgColor}, 1);
+    --card-color: rgba(${bgColor}, 0.8);
+    --border-color: rgba(${bgColor}, 0.6);
+    --font-color: rgba(${bgColor}, 1);
+}
+
+body {
+    border-size: border-box;
+    text-align: center;
+    background-color: var(--bg-color);
+    color: var(--font-color);
+    padding: 50px;
+    font-family: monospace;
+}
+
+/* PROFILE STYLES --------------------------------- */
+
+.aboutCont {
+    border: 8px outset var(--border-color);
+    height: 200px;
+    padding: 2px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.aboutCont > img {
+    content: "\A";
+    height: 60px;
+    width: 60px;
+    border: 6px solid var(--border-color);
+    background-color: var(--bg-color);
+    border-radius: 50%;
+}
+
+div {
+    border: 2px solid var(--border-color);
+}
+
+.section {
+    height: 600px;
+    display: grid;
+}
+
+
+/* PORTFOLIO STYLES ------------------------------ */
+
+.portfolio {
+    display: grid;
+    gap: 20px;
+    grid-template-areas: 
+        'main main'
+        'second third';
+    grid-template-rows: 3fr 2fr;
+    grid-template-columns: 1fr 1fr;
+}
+
+.portfolio > .card:first-child {
+    grid-area: main;
+    height: 100%;
+}
+
+.portfolio > .card:nth-child(2) {
+    grid-area: second;
+}
+
+.portfolio > .card:nth-child(3) {
+    grid-area: third;
+}
+
+
+.card {
+    height: 200px;
+}
+
+.title {
+    height: 40px;
+    border-radius: 10px;
+}
+
+.image {
+    border: 2px solid var(--border-color);
+    height: 50%;
+    margin: 5px;
+    border-radius: 15px;
+}
+
+.text {
+    font-size: 1rem;
+}
+
+.url {
+
+}
+
+a:hover {
+    cursor: pointer;
+}
+
+a:active {
+
+}
+
+a:visited {
+
+} `
 }
 
 module.exports = cssGen;
